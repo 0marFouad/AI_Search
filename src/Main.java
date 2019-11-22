dc1704efcabbf1adea3447551aba42232d42db6d
safy97 <mostafasafy1997@gmail.com>
2019-11-23 00:27:58 +0200

    Revert "add comparable and equals"
    This reverts commit a7665076

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.util.HashSet;
-import java.util.Set;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -13,23 +10,18 @@ public class Main {
             }
         }
         grid[0][0] = 0;
-        Set<Puzzle> s = new HashSet<Puzzle>();
+
         Puzzle p = new Puzzle(grid);
         Puzzle[] childres = p.getChildren();
         for(int k=0;k<4;k++){
             for(int i=0;i<3;i++){
                 for(int j=0;j<3;j++){
                     System.out.print(childres[k].getGrid()[i][j] + " ");
-                    s.add(childres[k]);
                 }
                 System.out.println();
             }
             System.out.println("===============");
         }
-        System.out.println("mostafa"); // check connection
-        System.out.println(s.contains(childres[0])); // true
-        int myGrid [][] = {{0,1,2},{3,4,5},{6,7,8}};
-        System.out.println(s.contains(new Puzzle(myGrid))); //false
-
+        System.out.println("mostafa");
     }
 }
