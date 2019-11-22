cf9aa3d7b05bef3f06fcafbd53f9dc74116dc869
safy97 <mostafasafy1997@gmail.com>
2019-11-23 01:56:06 +0200

    Merge branch 'master' of /home/mostafa/IdeaProjects/AI_Search with conflicts.

--- src/BFS.java
+++ src/BFS.java
@@ -1,3 +1,45 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class BFS {
-    
+    private class Node{
+        private  Node parent;
+        private Puzzle child;
+        public Node(Node parent, Puzzle child){
+            this.parent = parent;
+            this.child = child;
+        }
+        public Node getParent() {
+            return parent;
+        }
+        public Puzzle getChild() {
+            return child;
+        }
+    }
+    private List <Puzzle> path;
+    private int cost;
+    private  List <Puzzle> expanded;
+    public BFS(Puzzle puzzle){
+        run(puzzle);
+    }
+    public void  reRun(Puzzle puzzle){
+        run(puzzle);
+    }
+
+    private  void run(Puzzle puzzle){
+        path = new LinkedList<>();
+        cost = 0;
+        expanded =new LinkedList<>();
+        LinkedList <Node> queue = new LinkedList<>();
+        queue.add(new Node(null,puzzle));
+        Set <Puzzle> set = new HashSet<>();
+        while (!queue.isEmpty()){
+            Node n = queue.remove();
+            Puzzle p =  n.getChild();
+        }
+    }
+
+
 }
